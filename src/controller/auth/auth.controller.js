679b4ef0e5a0414539f3b9b20ec26a189efc0920
psakshat <akshat.soni@parkhya.net>
2025-06-19 16:48:31 +0530

    error resolved.

--- src/controller/auth/auth.controller.js
+++ src/controller/auth/auth.controller.js
@@ -31,6 +31,7 @@ exports.register = async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
+    console.error("Register Error:", err);
     res.status(500).json({ message: "Server error" });
   }
 };
