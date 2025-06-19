2eb999bc108c5aec9df543606e0ef1f48a402855
psakshat <akshat.soni@parkhya.net>
2025-06-19 17:32:46 +0530

    error resolved.

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the Test Backend API");
+});
 // DB connectoin
 connectiondb(app, PORT);
 
