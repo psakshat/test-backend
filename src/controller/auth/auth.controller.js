5a596282551d986be81a57d5d1a447862b57270e
psakshat <akshat.soni@parkhya.net>
2025-06-19 17:55:43 +0530

    error resolved.

--- src/controller/auth/auth.controller.js
+++ src/controller/auth/auth.controller.js
@@ -59,6 +59,7 @@ exports.login = async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        phone: user.phone,
         role: user.role,
         specialty: user.specialty,
       },
